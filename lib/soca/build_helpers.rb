3482c4896eb304513c6050c9feebd36e8cb14a02
Tim Harper <timcharper@gmail.com>
2011-11-11 09:13:39 -0700

    Some build errors result in empty files. Be sure to keep on rebuilding

--- lib/soca/build_helpers.rb
+++ lib/soca/build_helpers.rb
@@ -5,7 +5,7 @@ module Soca
     # if destination modified more recently than source, it is assumed up-to-date, build not required.
     def build_files(pairs, &block)
       pairs.each do |src, dest|
-        next if File.exist?(dest) && File.mtime(src) < File.mtime(dest)
+        next if File.exist?(dest) && File.mtime(src) < File.mtime(dest) && File.size(dest) > 5
         FileUtils.mkdir_p(File.dirname(dest))
         yield src, dest
       end
